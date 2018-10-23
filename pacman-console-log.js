98ad81ed29e375bf1f369c91ec9e6c962e8476bf
MD MAMUNUR RASHID <netmamun@yahoo.com>
2018-10-23 17:46:37 -0400

    pacman done

---
+++ pacman-console-log.js
@@ -0,0 +1,103 @@
+console.log('Pac-Man eats ghosts for lunch.');
+
+// pacman-arrays.js
+var ghosts = ['Inky', 'Blinky', 'Pinky', 'Clyde'];
+
+console.log('There are ' + ghosts.length + ' ghosts.');
+console.log('Their names are: ');
+console.log(ghosts[0]);
+console.log(ghosts[1]);
+console.log(ghosts[2]);
+console.log(ghosts[3]);
+
+// pacman-objects-2.js
+var ghosts = {inky: 'Cyan', blinky: 'Red', pinky: 'Pink', clyde: 'Orange' };
+console.log(ghosts);
+
+var ghostName = 'clyde';
+ console.log(ghosts[ghostName]); // => 'Orange'
+
+
+
+ // pacman-for-loop.js
+var ghosts = ['Inky', 'Blinky', 'Pinky', 'Clyde'];
+
+for (var index = 0; index < ghosts.length; index++) {
+  console.log('Pac-Man eats ' + ghosts[index]);
+}
+
+// pacman-if-else.js
+var powerPelletEaten = false;
+var ghosts = 4;
+
+while (true) {
+  console.log('Ghosts remaining: ' + ghosts);
+
+  if (powerPelletEaten == false) {
+    console.log('Pac-Man eats the power pellet.');
+    powerPelletEaten = true;
+  } else if (ghosts > 0) {
+    console.log('Pac-Man eats a ghost.');
+    ghosts--;
+  } else {
+    break;
+  }
+}
+
+
+console.log('Pac-Man returns home after a long day in the maze.');
+
+
+
+
+// pacman-switch.js
+var ghost = 'Inky';
+var colour;
+
+switch (ghost) {
+  case 'Inky':
+    colour = 'Cyan';
+    break;
+  case 'Blinky':
+    colour = 'Red';
+    break;
+  case 'Pinky':
+    colour = 'Pink';
+    break;
+  case 'Clyde':
+    colour = 'Orange';
+    break;
+}
+
+console.log(ghost + ' is the colour ' + colour);
+
+
+
+// pacman-return.js
+function addScore(currentScore, eaten) {
+  switch (eaten) {
+  case 'dot':
+    scoreToAdd = 10;
+    break;
+  case 'powerPellet':
+    scoreToAdd = 50;
+    break;
+  case 'firstGhost':
+    scoreToAdd = 200;
+    break;
+  case 'secondGhost':
+    scoreToAdd = 400;
+    break;
+  }
+
+  return currentScore + scoreToAdd;
+}
+
+var score = 0;
+
+score = addScore(score, 'dot');
+score = addScore(score, 'dot');
+score = addScore(score, 'dot');
+score = addScore(score, 'powerPellet');
+score = addScore(score, 'firstGhost');
+console.log('Your score: ' + score);
